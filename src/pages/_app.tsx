22615576ce1acd325806e4dd9af90e2d39e0301a
tanerijun <tanerivince@gmail.com>
2023-04-20 00:30:31 +0800

    Add user authentication with Clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,20 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+      {...pageProps}
+    >
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
