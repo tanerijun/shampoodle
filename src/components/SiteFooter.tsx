cd14cfc808755faf3f357183586c895ba87ead2c
tanerijun <tanerivince@gmail.com>
2023-04-28 16:52:42 +0800

    Build footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,3 +1,29 @@
+import { Container, Link, Text } from "@nextui-org/react";
+
 export default function SiteFooter() {
-  return <footer>FOOTER</footer>;
+  return (
+    <footer>
+      <Container
+        css={{ display: "flex", justifyContent: "space-between", my: "$10" }}
+      >
+        <Text>
+          © 2023 Shampoodle -{" "}
+          <Link
+            href="https://vitaneri.com"
+            target="_blank"
+            css={{ "&:hover": { color: "$secondary" } }}
+          >
+            Vitaneri
+          </Link>
+        </Text>
+        <Link
+          href="https://github.com/tanerijun/shampoodle"
+          css={{ "&:hover": { color: "$secondary" } }}
+          target="_blank"
+        >
+          View Source
+        </Link>
+      </Container>
+    </footer>
+  );
 }
