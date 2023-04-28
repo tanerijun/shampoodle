73da21cbd511d00948d32b3960326c2f3a8089cd
tanerijun <tanerivince@gmail.com>
2023-04-28 10:58:07 +0800

    Fetch user from client

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -1,13 +1,52 @@
+import { Container, Loading, Text } from "@nextui-org/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { api } from "~/utils/api";
 
 export default function UserPage() {
+  const router = useRouter();
+  const { username } = router.query;
+
+  if (typeof username !== "string") {
+    typeof window !== "undefined" && void router.push("/user/"); // 404
+    return;
+  }
+
+  const { data: user, isLoading } = api.user.getByUsername.useQuery({
+    username,
+  });
+
+  if (isLoading) {
+    return (
+      <Container
+        fluid
+        css={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Loading size="lg">
+          <Text>Finding shower thoughts from all over the world...</Text>
+        </Loading>
+      </Container>
+    );
+  }
+
+  if (!user) {
+    typeof window !== "undefined" && void router.push("/user/"); // 404
+    return;
+  }
+
   return (
     <>
       <Head>
-        <title>User | Shampoodle</title>
+        <title>{user.username} | Shampoodle</title>
       </Head>
       <main>
-        <h1>User Page</h1>
+        <h1>{user.name}</h1>
       </main>
     </>
   );
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,25 +1,27 @@
 import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  me: publicProcedure.query(async ({ ctx }) => {
-    const { userId } = ctx;
+  getByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const { username } = input;
 
-    if (!userId) {
-      return null;
-    }
+      const [user] = await clerkClient.users.getUserList({
+        username: [username],
+      });
 
-    const user = await clerkClient.users.getUser(userId);
+      if (!user) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "User not found." });
+      }
 
-    if (!user) {
-      return null;
-    }
-
-    return {
-      id: user.id,
-      name: `${user.firstName || ""} ${user.lastName || ""}`.trim(),
-      username: user.username,
-      profilePicture: user.profileImageUrl,
-    };
-  }),
+      return {
+        id: user.id,
+        name: `${user.firstName || ""} ${user.lastName || ""}`.trim(),
+        username: user.username,
+        profilePicture: user.profileImageUrl,
+      };
+    }),
 });
